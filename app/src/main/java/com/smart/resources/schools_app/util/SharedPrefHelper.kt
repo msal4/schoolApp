5f6e40bd940559386d6335e77981382933425ee4
ammaralwaeli <ammaralwaeli6@gmail.com>
2019-12-30 21:09:11 +0300

    finish absence and exam (Connect with backend)3

---
+++ app/src/main/java/com/smart/resources/schools_app/util/SharedPrefHelper.kt
@@ -0,0 +1,42 @@
+package com.smart.resources.schools_app.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPrefHelper private constructor(context: Context) {
+    var imgUri: String?
+        get() = mSharedPreferences.getString(IMG_URI, null)
+        set(accessToken) {
+            mSharedPreferences.edit().putString(IMG_URI, accessToken).apply()
+        }
+
+    private val mSharedPreferences: SharedPreferences
+    var accessToken: String?
+        get() = mSharedPreferences.getString(ACCESS_TOKEN, null)
+        set(accessToken) {
+            mSharedPreferences.edit().putString(ACCESS_TOKEN, accessToken)
+                .apply()
+        }
+
+    companion object {
+        private var instance: SharedPrefHelper? = null
+        private const val PREF_NAME = "mySettingsPref"
+        private const val IMG_URI = "myImage"
+        private const val ACCESS_TOKEN = "ACCESS_TOKEN"
+        fun init(context: Context) {
+            instance = SharedPrefHelper(context)
+        }
+
+        fun getInstance(): SharedPrefHelper? {
+            if (instance == null) throw NullPointerException()
+            return instance
+        }
+    }
+
+    init {
+        mSharedPreferences = context.getSharedPreferences(
+            PREF_NAME,
+            Context.MODE_PRIVATE
+        )
+    }
+}
