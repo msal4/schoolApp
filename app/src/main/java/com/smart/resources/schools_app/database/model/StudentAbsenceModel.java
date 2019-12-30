7a69b47d52717d17f25d95e10d0b7c83eb31e6f4
ammaralwaeli <ammaralwaeli6@gmail.com>
2019-12-30 21:43:02 +0300

    -added student absence model

---
+++ app/src/main/java/com/smart/resources/schools_app/database/model/StudentAbsenceModel.java
@@ -0,0 +1,43 @@
+package com.smart.resources.schools_app.database.model;
+import com.google.gson.annotations.SerializedName;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalDateTime;
+
+public class StudentAbsenceModel {
+
+	@SerializedName("absence_id")
+	private int absenceId;
+
+	@SerializedName("subject_name")
+	private String subjectName;
+
+	@SerializedName("student_id")
+	private int studentId;
+
+	@SerializedName("teacher_id")
+	private int teacherId;
+
+	@SerializedName("absence_date")
+	private LocalDateTime absenceDate;
+
+	public int getAbsenceId(){
+		return absenceId;
+	}
+
+	public String getSubjectName(){
+		return subjectName;
+	}
+
+	public int getStudentId(){
+		return studentId;
+	}
+
+	public int getTeacherId(){
+		return teacherId;
+	}
+
+	public LocalDateTime getAbsenceDate(){
+		return absenceDate;
+	}
+}
