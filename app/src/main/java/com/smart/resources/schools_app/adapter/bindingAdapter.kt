5af89db259a3166ef68f4a110aacb4a93320733c
ammaralwaeli <ammaralwaeli6@gmail.com>
2019-12-30 21:08:26 +0300

    finish absence and exam (Connect with backend)2

--- app/src/main/java/com/smart/resources/schools_app/adapter/bindingAdapter.kt
+++ app/src/main/java/com/smart/resources/schools_app/adapter/bindingAdapter.kt
@@ -1,8 +1,10 @@
 package com.smart.resources.schools_app.adapter
 
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.orhanobut.logger.Logger
 import com.smart.resources.schools_app.util.dateDisFormatter
 import org.threeten.bp.LocalDate
@@ -24,4 +26,14 @@ fun setDate(textView: TextView, date: LocalDateTime){
         Logger.e("binding error: ${e.message}")
     }
 
+}
+
+
+
+@BindingAdapter("android:srcUrl")
+fun loadImageUrl(iv: ImageView, url: String) {
+    Glide
+        .with(iv.context)
+        .load(url)
+        .into(iv)
 }
--- app/src/main/java/com/smart/resources/schools_app/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/smart/resources/schools_app/ui/activity/HomeActivity.kt
@@ -1,14 +1,21 @@
 package com.smart.resources.schools_app.ui.activity
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.smart.resources.schools_app.R
+import com.smart.resources.schools_app.adapter.loadImageUrl
 import com.smart.resources.schools_app.databinding.ActivityHomeBinding
+import com.smart.resources.schools_app.util.IntentHelper
 import com.smart.resources.schools_app.util.Section
+import com.smart.resources.schools_app.util.SharedPrefHelper
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding:ActivityHomeBinding
@@ -23,7 +30,36 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        SharedPrefHelper.getInstance()?.imgUri?.let {
+            loadImageUrl(binding.circleImageView,it)
+
+        }
+
+
+
+
     }
+
+
+    fun selectImage(view: View){
+
+
+
+        IntentHelper.selectImage(this)
+
+        binding.circleImageView
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(IntentHelper.GET_IMAGE_REQUEST==requestCode && resultCode== Activity.RESULT_OK){
+            val uri=IntentHelper.getImage(data)
+            SharedPrefHelper.getInstance()?.imgUri=uri.toString()
+            loadImageUrl(binding.circleImageView,uri.toString())
+        }
+    }
+
 
     fun navigate(view: View) {
 
