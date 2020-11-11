d6b985786fc769f4514521e3be504e6d493365d0
haytham-c01 <0TU8KR21mbBs>
2020-11-11 17:21:15 +0300

    - swapped section and classes in display

--- app/src/main/java/com/smart/resources/schools_app/features/profile/ClassInfoModel.kt
+++ app/src/main/java/com/smart/resources/schools_app/features/profile/ClassInfoModel.kt
@@ -1,13 +1,11 @@
 package com.smart.resources.schools_app.features.profile
 
-import com.haytham.coder.extensions.unicodeWrap
-
 
 data class ClassInfoModel(
     val classId: String,
     val className: String,
     val sectionName: String
 ){
-    val getClassSection get() = "${className.unicodeWrap()} - ${sectionName.unicodeWrap()}"
+    val getClassSection get() = "$className - $sectionName"
     override fun toString(): String = getClassSection
 }
